4dc14b655f1f651f44838b4147ce8f7e397b8b4c
guilhermeguedes <guilhermeguedes.gag@gmail.com>
2024-03-18 00:12:28 -0300

    Codando ainda

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
+
+    private int idade;
+    private float salario;
+
+    private String localizacao;
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
 }
+
+
